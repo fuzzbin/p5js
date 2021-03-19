e794cd21a57c1f07a67a4d14a98dd73eacd54307
fuzzbin <fuzzbin@gmail.com>
2021-03-19 19:59:53 +0100

    Nytt eksempel

---
+++ Fjaerkraft/sketch.js
@@ -0,0 +1,11 @@
+// Fjærkraft
+// Tom Jarle Christiansen - CC BY SA - 2021
+// Eksemplet er basert på koden i filmen: https://www.youtube.com/watch?v=Rr-5HiXquhw
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
