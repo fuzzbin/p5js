370c88b3cdcff3a59dead170520226b92158578a
fuzzbin <fuzzbin@gmail.com>
2021-03-20 14:47:34 +0100

    Flere kommentarer

--- Fjaerkraft/sketch.js
+++ Fjaerkraft/sketch.js
@@ -1,6 +1,7 @@
 // Fjærkraft
 // Tom Jarle Christiansen - CC BY SA - 2021
-// Eksemplet er basert på koden i filmen: https://www.youtube.com/watch?v=Rr-5HiXquhw
+// Eksemplet er basert på koden laget av Daniel Shiffman
+// i filmen: https://www.youtube.com/watch?v=Rr-5HiXquhw
 
 function setup() {
   createCanvas(1000, 1000);
