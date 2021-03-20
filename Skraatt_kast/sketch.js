0352153d0d2d2af5ab503f438abb2ba56281f5b1
fuzzbin <fuzzbin@gmail.com>
2021-03-20 15:33:30 +0100

    Kommentert kode

--- Skraatt_kast/sketch.js
+++ Skraatt_kast/sketch.js
@@ -4,40 +4,39 @@
 
 class Partikkel {
   constructor(x, y, d, v_x, v_y) {
-    this.x = x;
-    this.y = y;
+    this.p = createVector(x, y);
+    this.v = createVector(v_x, v_y);
+    this.a = createVector(0, 0.2); // Litt gravitasjon
     this.d = d;
-    this.v_x = v_x;
-    this.v_y = v_y;
-    this.cr = Math.random() * 255;
-    this.cg = Math.random() * 255;
-    this.cb = Math.random() * 255;
+    this.cr = parseInt(Math.random() * 255);
+    this.cg = parseInt(Math.random() * 255);
+    this.cb = parseInt(Math.random() * 255);
   }
 
   tegn() {
-    this.v_y += g;
-    this.x += this.v_x;
-    this.y += this.v_y;
+    this.v.add(this.a);
+    this.p.add(this.v);
     fill(this.cr, this.cg, this.cb); // Fargelegger partikkelen
     noStroke();
-    circle(this.x, this.y, this.d);
+    circle(this.p.x, this.p.y, this.d);
   }
 }
 
 // Init
 N = 100; // Antall partikler
-let g = 0.04; // "Gravitasjon" nedover
 let m = 2;
-
-// Lager N partikler
 let partikler = [];
-for ( let i = 0; i < N; i++ ) {
-  partikler.push(new Partikkel(100, 100, 3, Math.random() * 10, -2 * Math.random()));
-}
+
+
 
 function setup() {
   createCanvas(1000, 600);
   background(220);
+
+  // Lager N partikler
+  for ( let i = 0; i < N; i++ ) {
+    partikler.push(new Partikkel(100, 100, 3, Math.random() * 10, -2 * Math.random()));
+  }
 }
 
 function draw() {
